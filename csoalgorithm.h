4c3161e32bef27f28847605c2cd9b152d338446f
Hikosso <onikmarcin@wp.pl>
2023-03-25 23:24:43 +0100

    Refactoring code

--- csoalgorithm.h
+++ csoalgorithm.h
@@ -1,35 +1,36 @@
 #ifndef CSOALGORITHM_H
 #define CSOALGORITHM_H
 #include <vector>
+
 #include "testfunctions.h"
 
-class CSOAlgorithm
-{
+class CSOAlgorithm {
 private:
-    int numberOfCockroaches;
-    int dim;
-    int maxIterations;
+    int    numberOfCockroaches;
+    int    dim;
+    int    maxIterations;
     double lowerBound;
     double upperBound;
     double visual;
     double eps;
     double w;
-    double (*testFunction)(std::vector<double>,int);
+    double (*testFunction)(std::vector<double>, int);
 
     std::vector<double> generateRandomSolution(int dim, double lowerBound, double upperBound);
-    std::vector<double> findGlobalOptimum(std::vector<std::vector<double>> cockroaches, double (*testFunction)(std::vector<double>, int));
-    std::vector<double> updatePosition(std::vector<double> position, std::vector<double> optimumPosition, double stepSize);
+    std::vector<double> findGlobalOptimum(std::vector<std::vector<double>> cockroaches,
+                                          double (*testFunction)(std::vector<double>, int));
+    std::vector<double> updatePosition(std::vector<double> position,
+                                       std::vector<double> optimumPosition, double stepSize);
     double diffCockroaches(std::vector<double> cockroaches_I, std::vector<double> cockroaches_II);
-    bool isLocalOptimum(std::vector<double> localOpt, std::vector<double> pos);
+    bool   isLocalOptimum(std::vector<double> localOpt, std::vector<double> pos);
     std::vector<double> updatePostionInLight(std::vector<double> position);
 
-
 public:
-    CSOAlgorithm(int numberOfCockroaches, int dim, int maxIterations, double lowerBound, double upperBound, double visual, double eps, double w,
-                 double (*testFunction)(std::vector<double>,int));
-
+    CSOAlgorithm(int numberOfCockroaches, int dim, int maxIterations, double lowerBound,
+                 double upperBound, double visual, double eśps, double w,
+                 double (*testFunction)(std::vector<double>, int));
 
     std::vector<double> calculateGlobalOptimum();
 };
 
-#endif // CSOALGORITHM_H
+#endif  // CSOALGORITHM_H
--- testfunctions.cpp
+++ testfunctions.cpp
@@ -1,32 +1,34 @@
 #include "testfunctions.h"
+
+#include <math.h>
+
 #include <iostream>
 #include <vector>
-#include <math.h>
 
 const double PI = 3.14159265;
 
-double Schwefel(std::vector <double> x, int dim){
-    double sum = 0.0;
+double Schwefel(std::vector<double> x, int dim) {
+    double       sum           = 0.0;
     const double schwefelConst = 418.9829;
 
-    for(int i = 1 ; i <= dim ; i++){
-        sum += x[i-1] * sin( sqrt( fabs(x[i-1]) ) );
+    for (int i = 1; i <= dim; i++) {
+        sum += x[i - 1] * sin(sqrt(fabs(x[i - 1])));
     }
     return schwefelConst * dim - sum;
 }
 
-double Rastring(std::vector <double> x, int dim){
+double Rastring(std::vector<double> x, int dim) {
     double sum = 0.0;
-    for(int i = 1; i <= dim; i++) {
-        sum += (x[i-1] * x[i-1]) - 10 * cos(PI * 2 * x[i-1]);
+    for (int i = 1; i <= dim; i++) {
+        sum += (x[i - 1] * x[i - 1]) - 10 * cos(PI * 2 * x[i - 1]);
     }
     return 10 * dim + sum;
 }
 
-double HiperElipsoide(std::vector <double> x, int dim ){
+double HiperElipsoide(std::vector<double> x, int dim) {
     double sum = 0.0;
-    for(int i = 1; i <= dim; i++) {
-        sum += i * (x[i-1] * x[i-1]);
+    for (int i = 1; i <= dim; i++) {
+        sum += i * (x[i - 1] * x[i - 1]);
     }
     return sum;
 }
