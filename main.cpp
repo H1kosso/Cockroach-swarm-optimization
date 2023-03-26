960e135fbfca5c1a1d30b05b61639c9590b6b569
Hikosso <onikmarcin@wp.pl>
2023-03-26 21:32:47 +0200

    Created GUI

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
-#include <math.h>
-#include <testfunctions.h>
-
-#include <algorithm>
+#include <QApplication>
 #include <iostream>
 #include <vector>
 
 #include "csoalgorithm.h"
+#include "mainwindow.h"
+#include "testfunctions.h"
 
-int main() {
-    int    numberOfCockroaches = 500;
-    int    maxIterations       = 200;
-    double eps                 = 0.001;
-    double visual              = 0.1;
-    int    dim                 = 2;
-    double W                   = 0.1;
-    double lowerBound          = -500;
-    double upperBound          = 500;
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow   w;
+    w.show();
+    w.setDefaultRadioButton();
 
-    double (*testFunction)(std::vector<double>, int) = &Schwefel;
-    CSOAlgorithm algorytm(numberOfCockroaches, dim, maxIterations, lowerBound, upperBound, visual,
-                          eps, W, testFunction);
-    std::vector<double> solution = algorytm.calculateGlobalOptimum();
-    std::cout << "Znaleziono rozwiazanie: " << std::endl;
-    std::cout << "F(x) = " << Schwefel(solution, dim) << " dla x { ";
-    for (auto x : solution) {
-        std::cout << x << " ";
-    }
-    std::cout << "}" << std::endl;
-
-    return 0;
+    return a.exec();
 }
